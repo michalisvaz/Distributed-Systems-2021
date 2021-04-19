c42eda5056e25c0cd9de7da554f4c7941d32913d
Dimitris Delikouras <dimitris.delikouras@gmail.com>
2021-04-20 00:36:43 +0300

    initial sockets(not working)

--- framework/BrokerMain.java
+++ framework/BrokerMain.java
@@ -1,2 +1,8 @@
+import components.Broker;
+
 public class BrokerMain {
+    public static void main(String[] args) {
+        Broker broker = new Broker(4321);
+        broker.openBroker();
+    }
 }
--- framework/components/Broker.java
+++ framework/components/Broker.java
@@ -1,20 +1,57 @@
 package components;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Broker {
 
-    private final String IP;
+    private final int port;
+    ServerSocket brokerSocket;
+    Socket connection = null;
 
     /**
      * There will be many more properties
      *
-     * @param IP broker's ip address
+     * @param port broker's ip address
      */
-    public Broker(String IP) {
-        this.IP = IP;
+
+    public Broker(int port) {
+        this.port = port;
     }
 
-    public String getIP() {
-        return IP;
+
+    public int getPort() {
+        return port;
+    }
+
+    public void openBroker(){
+
+        try {
+
+            brokerSocket = new ServerSocket(port);
+
+            while (true) {
+                connection = brokerSocket.accept();
+
+                Thread t = new ClientHandler(connection);
+
+                t.start();
+
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            try {
+                brokerSocket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+
+
     }
 
 }
