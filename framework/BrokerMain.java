f1b17c2843508bed72e13cb93e75e4616e17b18c
michalisvaz <35351243+michalisvaz@users.noreply.github.com>
2021-04-28 22:09:25 +0300

    Update BrokerMain.java

--- framework/BrokerMain.java
+++ framework/BrokerMain.java
@@ -4,5 +4,10 @@ public class BrokerMain {
     public static void main(String[] args) {
         Broker broker = new Broker(4321);
         broker.openBroker();
+        // Ena Servsock gia clietns ki ena gia brokers
+        // HashMap fullname -> VideoFile
+        // HashMap hashtag -> fullname
+        // HashMap channel -> fullname
+        
     }
 }
