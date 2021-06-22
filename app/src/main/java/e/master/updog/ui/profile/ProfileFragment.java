c0b2bec9c95674f232574c7b734a51fe45a93429
Bill Ballas <ballas@programmer.net>
2021-06-23 00:03:51 +0300

    icon, colors and upload xml

--- app/src/main/java/e/master/updog/ui/profile/ProfileFragment.java
+++ app/src/main/java/e/master/updog/ui/profile/ProfileFragment.java
@@ -1,6 +1,5 @@
 package e.master.updog.ui.profile;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,17 +9,13 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import e.master.updog.LoginActivity;
 import e.master.updog.MainActivity;
 import e.master.updog.R;
 import e.master.updog.databinding.FragmentProfileBinding;
